40da2a663dae116d9dbf7e23b9a322d47631c592
Suhwan0818 <tnghks1997@gmail.com>
2022-12-18 22:51:18 +0900

    💚 animation delete

--- src/components/Infomation.js
+++ src/components/Infomation.js
@@ -1,13 +1,8 @@
-import useScrollFadeIn from '../hooks/useScrollFadeIn';
-
 const Infomation = () => {
-  const animatedItem = useScrollFadeIn();
   return (
     <>
       <article className="w-full p-4 text-center bg-slate-500 border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700 text-slate-50 m-13 relative">
-        <p {...animatedItem} className="font-bold">
-          기본정보
-        </p>
+        <p className="font-bold">기본정보</p>
       </article>
     </>
   );
